4b010e59a6b7ea653fe579699df9acc37a673a11
Ilia Valchenko <ilia.valchenko.epam@gmail.com>
2016-11-13 23:20:11 +0300

    Add the corrected version  of LINQ queries.

---
+++ Day6_LINQ/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private List<User> userListFirst = new List<User>
+        {
+            new User
+            {
+                Age = 21,
+                Gender = Gender.Man,
+                Name = "User1",
+                Salary = 21000
+            },
+
+            new User
+            {
+                Age = 30,
+                Gender = Gender.Female,
+                Name = "Liza",
+                Salary = 30000
+            },
+
+            new User
+            {
+                Age = 18,
+                Gender = Gender.Man,
+                Name = "Max",
+                Salary = 19000
+            },
+            new User
+            {
+                Age = 32,
+                Gender = Gender.Female,
+                Name = "Ann",
+                Salary = 36200
+            },
+            new User
+            {
+                Age = 45,
+                Gender = Gender.Man,
+                Name = "Alex",
+                Salary = 54000
+            }
+        };
+
+        private List<User> userListSecond = new List<User>
+        {
+            new User
+            {
+                Age = 23,
+                Gender = Gender.Man,
+                Name = "Max",
+                Salary = 24000
+            },
+
+            new User
+            {
+                Age = 30,
+                Gender = Gender.Female,
+                Name = "Liza",
+                Salary = 30000
+            },
+
+            new User
+            {
+                Age = 23,
+                Gender = Gender.Man,
+                Name = "Max",
+                Salary = 24000
+            },
+            new User
+            {
+                Age = 32,
+                Gender = Gender.Female,
+                Name = "Kate",
+                Salary = 36200
+            },
+            new User
+            {
+                Age = 45,
+                Gender = Gender.Man,
+                Name = "Alex",
+                Salary = 54000
+            },
+            new User
+            {
+                Age = 28,
+                Gender = Gender.Female,
+                Name = "Kate",
+                Salary = 21000
+            }
+        };
+
+        [TestMethod]
+        public void SortByName()
+        {
+            var expectedData = userListFirst[4];
+
+            var actualDataFirstList = userListFirst.OrderBy(u => u.Name).ToList();
+
+            Assert.IsTrue(actualDataFirstList[0].Equals(expectedData));
+        }
+
+        [TestMethod]
+        public void SortByNameDescending()
+        {
+            var expectedData = userListFirst[0];
+
+            var actualDataFirstList = userListFirst.OrderByDescending(u => u.Name).ToList();
+
+            Assert.IsTrue(actualDataFirstList[0].Equals(expectedData));
+        }
+
+        [TestMethod]
+        public void SortByNameAndAge()
+        {
+            var expectedData = userListSecond[5];
+
+            var actualDataSecondList = userListSecond.OrderBy(u => u.Name).ThenBy(u => u.Age).ThenBy(u => u).ToList();
+            Assert.IsTrue(actualDataSecondList[0].Equals(expectedData));
+        }
+
+        [TestMethod]
+        public void RemovesDuplicate()
+        {
+            var actualDataSecondList = new List<User>();
+            var expectedData = new List<User> {userListSecond[0], userListSecond[1], userListSecond[3], userListSecond[4],userListSecond[5]};
+
+            actualDataSecondList = userListSecond.Select(u => u).Distinct().ToList();
+
+            CollectionAssert.AreEqual(expectedData, actualDataSecondList);
+        }
+
+        [TestMethod]
+        public void ReturnsDifferenceFromFirstList()
+        {
+            var actualData = new List<User>();
+            var expectedData = new List<User> { userListFirst[0], userListFirst[2], userListFirst[3] };
+
+            actualData = userListFirst.Except(userListSecond).ToList();
+
+            CollectionAssert.AreEqual(expectedData, actualData);
+        }
+
+        [TestMethod]
+        public void SelectsValuesByNameMax()
+        {
+            var actualData = new List<User>();
+            var expectedData = new List<User> { userListSecond[0], userListSecond[2] };
+
+            var maxValue = userListSecond.Max(u => u.Name);
+            actualData = userListSecond.Where(u => u.Name == maxValue).ToList();
+
+            CollectionAssert.AreEqual(expectedData, actualData);
+        }
+
+        [TestMethod]
+        public void ContainOrNotContainName()
+        {
+            var isContain = false;
+
+            //name max
+            isContain = userListSecond.Select(u => u.Name).Contains("Max");
+
+            Assert.IsTrue(isContain);
+
+            // name obama
+            isContain = userListSecond.Select(u => u.Name).Contains("Obama");
+            Assert.IsFalse(isContain);
+        }
+
+        [TestMethod]
+        public void AllListWithName()
+        {
+            var isAll = true;
+
+            //name max 
+            //ToDo Add code for second list
+
+            Assert.IsTrue(isAll);
+        }
+
+        [TestMethod]
+        public void ReturnsOnlyElementByNameMax()
+        {
+            var actualData = new User();
+            
+            try
+            {
+                //name Max
+                actualData = userListFirst.Single(u => u.Name == "Max");
+            }
+            catch (InvalidOperationException ie)
+            {
+                Assert.AreEqual("Sequence contains more than one matching element", ie.Message);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("Unexpected exception of type {0} caught: {1}", e.GetType(), e.Message);
+            }
+        }
+
+        [TestMethod]
+        public void ReturnsOnlyElementByNameNotOnList()
+        {
+            var actualData = new User();
+
+            try
+            {
+                //name Ldfsdfsfd
+                actualData = userListFirst.Where(u => u.Name == "Ldfsdfsfd").Single();
+
+                if(actualData == null)
+                    Console.Write("test");
+            }
+            catch (InvalidOperationException ie)
+            {
+                Assert.AreEqual("Последовательность не содержит элементов", ie.Message);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("Unexpected exception of type {0} caught: {1}", e.GetType(), e.Message);
+            }
+        }
+
+        [TestMethod]
+        public void ReturnsOnlyElementOrDefaultByNameNotOnList()
+        {
+            var actualData = new User();
+
+             //name Ldfsdfsfd
+            actualData = userListFirst.SingleOrDefault(u => u.Name == "Ldfsdfsfd");
+
+            Assert.IsTrue(actualData == null);
+        }
+
+
+        [TestMethod]
+        public void ReturnsTheFirstElementByNameNotOnList()
+        {
+            var actualData = new User();
+
+            try
+            {
+                //name Ldfsdfsfd
+                actualData = userListFirst.First(u => u.Name == "Ldfsdfsfd");
+            }
+            catch (InvalidOperationException ie)
+            {
+                Assert.AreEqual("Последовательность не содержит соответствующий элемент", ie.Message);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("Unexpected exception of type {0} caught: {1}", e.GetType(), e.Message);
+            }
+        }
+
+        [TestMethod]
+        public void ReturnsTheFirstElementOrDefaultByNameNotOnList()
+        {
+            var actualData = new User();
+
+            //name Ldfsdfsfd
+            actualData = userListFirst.FirstOrDefault(u => u.Name == "Ldfsdfsfd");
+
+            Assert.IsTrue(actualData == null);
+        }
+
+        [TestMethod]
+        public void GetMaxSalaryFromFirst()
+        {
+            var expectedData = 54000;
+            var actualData = new User();
+
+            var maxSalary = userListFirst.Max(u => u.Salary);
+            actualData = userListFirst.Single(u => u.Salary == maxSalary);
+
+            Assert.IsTrue(expectedData == actualData.Salary);
+        }
+
+        [TestMethod]
+        public void GetCountUserWithNameMaxFromSecond()
+        {
+            var expectedData = 2;
+            var actualData = 0;
+
+            actualData = userListSecond.Count(u => u.Name == "Max");
+
+            Assert.IsTrue(expectedData == actualData);
+        }
+
+        [TestMethod]
+        public void Join()
+        {
+            var NameInfo = new[]
+            {
+                new {name = "Max", Info = "info about Max"},
+                new {name = "Alan", Info = "About Alan"},
+                new {name = "Alex", Info = "About Alex"}
+            }.ToList();
+
+            var expectedData = 3;
+            var actualData = -1;
+
+            var users = from n in NameInfo
+                join u in userListSecond on n.name equals u.Name
+                select new User();
+
+            actualData = users.Count();
+                
+            Assert.IsTrue(expectedData == actualData);
+        }
+    }
+}
